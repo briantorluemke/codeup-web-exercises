137e73372fb448f0485cdd037fa0861d7ba63146
Brian Torluemke <briantorluemke@gmail.com>
2018-03-19 13:17:57 -0500

    Moved sign in to right side of navbar

---
+++ js/external.js
@@ -0,0 +1,41 @@
+console.log("Hello from external JavaScript")
+//
+// alert("Welcome! Please select your schedule options.")
+//
+// var isClassFull = confirm("Is this class full?")
+//
+//
+// var isConflicting = confirm("Does this class conflict with your current schedule?")
+//
+// var enroll = (isClassFull && isConflicting)
+//
+// alert(enroll)
+//
+// console.log(enroll)
+
+var username = prompt("Please enter a username")
+
+alert(username + " is your username")
+
+console.log("User's username is " + username)
+
+var password = prompt("Please enter a password")
+
+alert(password + " is your password")
+
+isUsernameLengthCorrect = username.length <= 20;
+
+console.log("Username is long enough:" + isUsernameLengthCorrect)
+
+isPasswordLengthCorrect = password.length >= 5;
+
+console.log("Password is correct length:" + isPasswordLengthCorrect)
+
+
+
+
+
+
+
+
+
