4ba678b683e52855cdb9597192f253803058c804
Brian Torluemke <briantorluemke@gmail.com>
2018-03-20 09:29:45 -0500

    Created new JS files and tested alerts etc

--- js/external.js
+++ js/external.js
@@ -1,4 +1,4 @@
-console.log("Hello from external JavaScript")
+console.log("Hello from external JavaScript");
 //
 // alert("Welcome! Please select your schedule options.")
 //
@@ -13,26 +13,39 @@ console.log("Hello from external JavaScript")
 //
 // console.log(enroll)
 
-var username = prompt("Please enter a username")
+var username = prompt("Please enter a username");
 
-alert(username + " is your username")
+alert(username + " is your username");
 
-console.log("User's username is " + username)
+console.log("User's username is " + username);
 
-var password = prompt("Please enter a password")
+var password = prompt("Please enter a password");
 
-alert(password + " is your password")
+alert(password + " is your password");
 
 isUsernameLengthCorrect = username.length <= 20;
 
-console.log("Username is long enough:" + isUsernameLengthCorrect)
+console.log("Username is long enough:" + isUsernameLengthCorrect);
 
 isPasswordLengthCorrect = password.length >= 5;
 
-console.log("Password is correct length:" + isPasswordLengthCorrect)
+console.log("Password is correct length:" + isPasswordLengthCorrect);
 
+// movie rental
 
-
+// var littleMermaidDays = prompt("How many days did we rent The Little Mermaid for?");
+//
+// var brotherBearDays = prompt("How many days did we rent Brother Bear for?");
+//
+// var herculesDays = prompt("How many days did we rent Hercules for?");
+//
+// var pricePerDay = 3;
+//
+// var totalDays = littleMermaidDays + brotherBearDays + herculesDays;
+//
+// littleMermaidDays = parseInt(littleMermaidDays);
+// brotherBearDays = parseInt(brotherBearDays);
+// herculesDays = parseInt(herculesDays);
 
 
 
