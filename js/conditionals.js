aaf664c5154f209c31ffd7d7a9f35da48d806707
Brian Torluemke <briantorluemke@gmail.com>
2018-03-21 12:23:16 -0500

    Finished exercise

--- js/conditionals.js
+++ js/conditionals.js
@@ -16,25 +16,35 @@
  * Can you refactor your code to use functions?
  */
 
-var enterNumber = confirm("Would you like to enter a number?");
-
-if (enterNumber) {
-        var number = prompt("Enter a number!");
-        parseInt(number);
-        var evenNumber = number % 2 === 0;
-        if (evenNumber) {
-            alert("Your number is even.")
-        } else {
-            alert("Your number is odd.")
-        }
-        var plus100 = number + 100;
-        alert (plus100);
-
-
-
-} else {
-    alert("Fine then.")
-}
+// var enterNumber = confirm("Would you like to enter a number?");
+//
+// function promptNumber(question) {
+//     var userInput = prompt(question);
+//     return parseFloat(userInput);
+// }
+//
+// if (enterNumber) {
+//         var number = promptNumber();
+//         var evenNumber = number % 2 === 0;
+//         if (evenNumber) {
+//             alert("Your number is even.")
+//         } else {
+//             alert("Your number is odd.")
+//         }
+//         var plus100 = (number + 100);
+//         parseInt(plus100);
+//         alert ("Your number plus 100 would be " + plus100);
+//         if (number > 0) {
+//             alert("Your number is positive.")
+//         } else {
+//             alert ("Your number is negative.")
+//         }
+//
+//
+//
+// } else {
+//     alert("Fine then.")
+// }
 
 
 
@@ -58,6 +68,26 @@ if (enterNumber) {
  * console.logging the function's return value
  */
 
+// function analyzeColor(x) {
+//     var colorBlue = (x === "blue");
+//     var colorRed = (x === "red");
+//     var colorCyan = (x === "cyan");
+//     console.log(x);
+//     if (colorBlue) {
+//         alert("Blue is the color of the sky.")
+//     } else if (colorRed) {
+//         alert("Strawberries are red")
+//     } else if (colorCyan) {
+//         alert("I don't know anything about cyan.")
+//     } else {
+//         alert ("Sorry, I don't know anything about that color (if it's even a color).")
+//     }
+//
+// }
+
+
+
+
 // Don't change the next two lines!
 // These lines create two variables for you:
 // - `colors`: a list of the colors of the rainbow
@@ -71,10 +101,40 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * You should see a different message everytime you refresh the page
  */
 
+
+// console.log(randomColor);
+// console.log(analyzeColor(randomColor));
+
+
 /**
  * TODO:
  * Refactor your above function to use a switch-case statement
  */
+
+
+// function analyzeColor(x) {
+//     var colorBlue = (x === "blue");
+//     var colorRed = (x === "red");
+//     var colorCyan = (x === "cyan");
+//     alert("Your color is " + x);
+//     switch(x) {
+//         case "blue":
+//             alert("Blue is the color of the sky.");
+//             break;
+//         case "red":
+//             alert("Strawberries are red");
+//             break;
+//         case "cyan":
+//             alert("I don't know anything about cyan.");
+//             break;
+//         default:
+//             alert ("Sorry, I don't know anything about that color.");
+//             break;
+//     }
+//
+// }
+//
+// analyzeColor(randomColor);
 
 /**
  * TODO:
@@ -82,6 +142,11 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * user to your `analyzeColor` function. Alert the return value from your
  * function to show it to the user.
  */
+
+
+// var x = prompt("What color would you like to test?");
+// analyzeColor(x);
+
 
 /* ########################################################################## */
 
@@ -105,6 +170,35 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * return value.
  */
 
+
+function calculateTotal(y , z) {
+    var totalPrice;
+    switch (y) {
+        case (0):
+            totalPrice = z;
+            break;
+        case (1):
+            totalPrice = z * 0.9;
+            break;
+        case (2):
+            totalPrice = z * 0.75;
+            break;
+        case (3):
+            totalPrice = z * 0.65;
+            break;
+        case (4):
+            totalPrice = z * 0.5;
+            break;
+        case (5):
+            totalPrice = 0;
+            break;
+    }
+    return totalPrice;
+}
+
+
+console.log(calculateTotal(2 , 100));
+
 /**
  * TODO:
  * Uncomment the line below to generate a random number between 0 and 6.
@@ -113,4 +207,6 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * price before the discount was, and what their price after the discount is.
  */
 // Generate a random number between 0 and 6
-// var luckyNumber = Math.floor(Math.random() * 6);
+var luckyNumber = Math.floor(Math.random() * 6);
+console.log(luckyNumber);
+console.log(calculateTotal(luckyNumber , 100));
